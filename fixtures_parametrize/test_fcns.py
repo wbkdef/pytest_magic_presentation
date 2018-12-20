0ddb1ae1905f45d19b27e8980830aea4a8088b25
Bruce <wbrucek@gmail.com>
2018-12-19 18:43:56 -0800

    Minor changes

--- fixtures_parametrize/test_fcns.py
+++ fixtures_parametrize/test_fcns.py
@@ -9,16 +9,15 @@ from fcns import \
 # both start with "test_"
 # This is how pytest
 # knows it's a test.
+@pytest.mark.slow
 def test_fib_base():
     assert fib(0) == 0
     assert fib(1) == 1
-@pytest.mark.slow
+
 def test_fib_ind():
     assert fib(3) == 2
     assert fib(6) == 9
-# We run the same test,
-# multiple times with
-# different values
+
 @pytest.mark.parametrize("n", [
     -1, -2, -10,
     3.6, 1.1,
